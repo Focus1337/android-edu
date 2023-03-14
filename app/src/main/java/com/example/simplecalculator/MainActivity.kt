ee66450056f5df65b7c0c885999b00728dcbd15a
Focus <maniaclord@bk.ru>
2023-03-15 00:36:34 +0300

    in progress: adding buttons

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.simplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var helloTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        helloTextView = findViewById(R.id.textView)
+
+        val plusButton: Button = findViewById(R.id.buttonPlus)
+
+        plusButton.setOnClickListener {
+            helloTextView.text = "hello guys"
+        }
+
     }
 }
