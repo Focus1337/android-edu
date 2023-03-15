2248390e21eeb1d488348f2edab3079a4a938046
Focus <maniaclord@bk.ru>
2023-03-15 19:17:50 +0300

    code refactor

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.simplecalculator
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -34,7 +33,6 @@ class MainActivity : AppCompatActivity() {
 
         val buttonGroup: Group = findViewById(R.id.buttonGroup)
 
-        var prevButton: Button = defaultButton
         var curButton: Button
         var arg1 = ""
         var arg2 = ""
@@ -43,51 +41,52 @@ class MainActivity : AppCompatActivity() {
 
         buttonGroup.setAllOnClickListener {
             curButton = it as Button
-            if (it == defaultButton) {
-                Toast.makeText(context, "DEFAULT BUTTON", Toast.LENGTH_SHORT).show()
-            } else if (curButton.text in numbers) {
-                if (curOperation == Operation.Unknown) arg1 += curButton.text
-                else arg2 += curButton.text
-                prevButton = curButton
-            } else if (curButton.text in calculator.operators) {
-                if (arg1.isNotEmpty()) {
-                    if (curOperation == Operation.Unknown) {
-                        prevButton = curButton
-                        curOperation = calculator.getOperator(curButton.text.toString())
-                    } else {
+            when {
+                it == defaultButton -> Toast.makeText(context, "DEFAULT BUTTON", Toast.LENGTH_SHORT)
+                    .show()
+
+                curButton.text in numbers -> {
+                    if (curOperation == Operation.Unknown) arg1 += curButton.text
+                    else arg2 += curButton.text
+                }
+
+                curButton.text in calculator.operators -> {
+                    if (arg1.isEmpty()) {
+                        Toast.makeText(context, "ARG1 is EMPTY", Toast.LENGTH_SHORT).show()
+                        return@setAllOnClickListener
+                    }
+                    if (curOperation != Operation.Unknown) {
                         Toast.makeText(
                             context,
                             "OPERATOR ALREADY CHOSEN: ${calculator.getOperator(curOperation)}",
                             Toast.LENGTH_SHORT
                         ).show()
+                        return@setAllOnClickListener
                     }
-                } else
-                    Toast.makeText(context, "ARG1 is EMPTY", Toast.LENGTH_SHORT).show()
-
-            } else if (curButton == equalButton) {
-                if (arg1.isEmpty()) {
-                    Toast.makeText(context, "ARG1 is EMPTY", Toast.LENGTH_SHORT).show()
-                    return@setAllOnClickListener
-                }
-                if (arg2.isEmpty()) {
-                    Toast.makeText(context, "ARG2 is EMPTY", Toast.LENGTH_SHORT).show()
-                    return@setAllOnClickListener
-                }
-                if (curOperation == Operation.Unknown) {
-                    Toast.makeText(context, "OPERATOR ISN'T CHOSEN", Toast.LENGTH_SHORT).show()
-                    return@setAllOnClickListener
+                    curOperation = calculator.getOperator(curButton.text.toString())
                 }
 
-                resultTextView.text =
-                    calculator.calculate(arg1.toInt(), curOperation, arg2.toInt())
+                curButton == equalButton -> when {
+                    arg1.isEmpty() -> {
+                        Toast.makeText(context, "ARG1 is EMPTY", Toast.LENGTH_SHORT).show()
+                        return@setAllOnClickListener
+                    }
+                    arg2.isEmpty() -> {
+                        Toast.makeText(context, "ARG2 is EMPTY", Toast.LENGTH_SHORT).show()
+                        return@setAllOnClickListener
+                    }
+                    curOperation == Operation.Unknown -> {
+                        Toast.makeText(context, "OPERATOR ISN'T CHOSEN", Toast.LENGTH_SHORT).show()
+                        return@setAllOnClickListener
+                    }
+                    else -> {
+                        resultTextView.text =
+                            calculator.calculate(arg1.toInt(), curOperation, arg2.toInt())
+                    }
+                }
 
-                prevButton = defaultButton
-                arg1 = ""
-                arg2 = ""
-                curOperation = Operation.Unknown
-
-            } else
-                Toast.makeText(context, "UNKNOWN OPERATION", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(context, "UNKNOWN OPERATION", Toast.LENGTH_SHORT).show()
+            }
 
             operationTextView.text =
                 "$arg1 ${
@@ -96,6 +95,12 @@ class MainActivity : AppCompatActivity() {
                         else -> calculator.getOperator(curOperation)
                     }
                 } $arg2"
+
+            if (curButton == equalButton) {
+                arg1 = ""
+                arg2 = ""
+                curOperation = Operation.Unknown
+            }
         }
     }
 }
