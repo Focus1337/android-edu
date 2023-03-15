82f8217c55b0bd56a1d7b9cf9efa08d2c88e2b7d
Focus <maniaclord@bk.ru>
2023-03-15 03:50:32 +0300

    Added buttons to group. Temp commit

--- app/src/main/java/com/example/simplecalculator/Calculator.kt
+++ app/src/main/java/com/example/simplecalculator/Calculator.kt
@@ -1,5 +1,8 @@
 package com.example.simplecalculator
 
+import android.content.Context
+import android.widget.Toast
+
 enum class Operation {
     Plus,
     Minus,
@@ -7,15 +10,18 @@ enum class Operation {
     Divide
 }
 
-class Calculator {
+class Calculator(context: Context) {
+    private val activity = context
+
     fun calculate(arg1: Int, operation: Operation, arg2: Int): String = when (operation) {
         Operation.Plus -> (arg1 + arg2).toString()
         Operation.Minus -> (arg1 - arg2).toString()
         Operation.Multiply -> (arg1 * arg2).toString()
         Operation.Divide -> {
-            if (arg2 == 0)
+            if (arg2 == 0) {
+                Toast.makeText(activity, "Нельзя делить на 0!", Toast.LENGTH_SHORT).show()
                 "Error"
-            else
+            } else
                 (arg1 / arg2).toString()
         }
     }
--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -1,33 +1,56 @@
 package com.example.simplecalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Group
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var helloTextView: TextView
+    private lateinit var operationTextView: TextView
     private lateinit var resultTextView: TextView
+
+    private fun Group.setAllOnClickListener(listener: View.OnClickListener?) {
+        referencedIds.forEach { id ->
+            rootView.findViewById<View>(id).setOnClickListener(listener)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val calculator = Calculator()
+        val calculator = Calculator(this@MainActivity)
 
-        helloTextView = findViewById(R.id.operationTextView)
+        operationTextView = findViewById(R.id.operationTextView)
         resultTextView = findViewById(R.id.resultTextView)
 
-        val plusButton: Button = findViewById(R.id.plusButton)
+//        val variableGroup: Group = findViewById(R.id.variableGroup)
+//        val operationGroup: Group = findViewById(R.id.operationGroup)
 
-        val arg1 = 12
-        val arg2 = 4
+        val buttonGroup: Group = findViewById(R.id.buttonGroup)
 
-        plusButton.setOnClickListener {
-            helloTextView.text = "$arg1 ${calculator.getOperationSign(Operation.Plus)} $arg2"
-            resultTextView.text = calculator.calculate(arg1, Operation.Plus, arg2)
+//        for (yes in operationGroup.referencedIds) {
+//            val govno: Button = findViewById(yes)
+//            Log.i("xd", govno.text.toString())
+//        }
+
+
+        buttonGroup.setAllOnClickListener {
+            Log.i("XDDD", it.toString())
         }
+
+
+//
+//        val plusButton: Button = findViewById(R.id.plusButton)
+//
+//        val arg1 = 12
+//        val arg2 = 12
+
+//        plusButton.setOnClickListener {
+//            operationTextView.text = "$arg1 ${calculator.getOperationSign(Operation.Plus)} $arg2"
+//            resultTextView.text = calculator.calculate(arg1, Operation.Plus, arg2)
+//        }
     }
-
-
-
 }
